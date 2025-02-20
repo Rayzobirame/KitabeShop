137adbb8b129a3be7207ee0d0bbd96c78046973d
RayzoBirame <rayzobirame96@gmail.com>
2025-02-20 11:28:57 +0000

    Premiere initialisation du projet KitabeShop

---
+++ catalogue_Service/src/test/java/com/kitabe/catalogue_service/TestCatalogueServiceApplication.java
@@ -0,0 +1,12 @@
+package com.kitabe.catalogue_service;
+
+import org.springframework.boot.SpringApplication;
+
+
+public class TestCatalogueServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(CatalogueServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
