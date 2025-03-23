794e5d7c0e5d398e08fdf9733830cf1add5153dc
RayzoBirame <rayzobirame96@gmail.com>
2025-03-23 17:57:34 +0000

    Enum liste contenant les differentes etapes de la commande notifier lors de la creation d'un evenement

---
+++ commande_service/src/main/java/com/kitabe/commande_service/domaine/CommandeEvenementType.java
@@ -0,0 +1,8 @@
+package com.kitabe.commande_service.domaine;
+
+public enum CommandeEvenementType {
+    COMMANDE_CREER,
+    COMMANDE_DELIVREE,
+    COMMANDE_ANNULEE,
+    COMMANDE_ECHEC_PROCESSUS
+}
