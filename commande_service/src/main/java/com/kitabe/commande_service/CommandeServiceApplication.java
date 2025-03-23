b995473df33d60b996c3ad5850e647e79947b94f
RayzoBirame <rayzobirame96@gmail.com>
2025-03-23 18:09:32 +0000

    J'ai ajoute l'annotation @EnableScheduling pour accepter les declenchement automatique des jobs

--- commande_service/src/main/java/com/kitabe/commande_service/CommandeServiceApplication.java
+++ commande_service/src/main/java/com/kitabe/commande_service/CommandeServiceApplication.java
@@ -3,11 +3,12 @@ package com.kitabe.commande_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableScheduling
 public class CommandeServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CommandeServiceApplication.class, args);
     }
