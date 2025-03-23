d4fd9245ecdffd639cfd9430a585b08b9305409a
RayzoBirame <rayzobirame96@gmail.com>
2025-03-23 18:03:12 +0000

    Fichier de migration sql flyway permettant la creation de lentite Commande_evenement

---
+++ commande_service/src/main/resources/db/migration/V2__creation_commande_evenements.sql
@@ -0,0 +1,12 @@
+CREATE SEQUENCE commande_evenements_id_seq START WITH 1 INCREMENT BY 50;
+
+CREATE TABLE commande_evenements(
+                              id             bigint DEFAULT nextval('commande_evenements_id_seq') NOT NULL,
+                              commande_num             text NOT NULL references commandes(commande_num) ,
+                              evenement_id            text      NOT NULL unique ,
+                              evenement_type          text      NOT NULL,
+                              payload                 text      NOT NULL,
+                              creation_date              timestamp  NOT NULL ,
+                              update_le              timestamp,
+                            primary key (id)
+);
