b0661072adb01bc2b4e6e752cabfa76b533bb649
RayzoBirame <rayzobirame96@gmail.com>
2025-03-25 18:33:18 +0000

    Ajout de l'annotation @EnabledScheduling permettant d'activier la planification sur l'application , et le @EnableSchedulingLock qui permet d'activer le verrou avec au minimum 10min d'attentes

--- commande_service/src/main/java/com/kitabe/commande_service/CommandeServiceApplication.java
+++ commande_service/src/main/java/com/kitabe/commande_service/CommandeServiceApplication.java
@@ -1,5 +1,7 @@
 package com.kitabe.commande_service;
 
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -8,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "10m") // Verrou maintenu au maximum 10 minutes
 public class CommandeServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CommandeServiceApplication.class, args);
