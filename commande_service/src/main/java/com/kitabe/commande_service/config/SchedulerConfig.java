672701d9e3947071823a10c7474735e85028cfa5
RayzoBirame <rayzobirame96@gmail.com>
2025-03-26 14:18:35 +0000

    Class de configuration de ShedLock sur l'application kitabeShop , elle permet d'activer la synchronisation des tâches planifiées avec ShedLock avec l'utilsation des base de données relationnel avec jdbc

---
+++ commande_service/src/main/java/com/kitabe/commande_service/config/SchedulerConfig.java
@@ -0,0 +1,40 @@
+package com.kitabe.commande_service.config;
+
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.sql.DataSource;
+
+/**
+ * Configuration pour ShedLock dans l'application KitabeShop.
+ * Cette classe active la synchronisation des tâches planifiées avec ShedLock et configure un {@link LockProvider}
+ * pour utiliser une base de données relationnelle via JDBC. ShedLock garantit qu'une tâche planifiée ne s'exécute
+ * que sur une seule instance dans un environnement distribué.
+ */
+@Configuration
+//@EnableSchedulerLock(defaultLockAtMostFor = "10m")
+public class SchedulerConfig {
+
+    /**
+     * Crée un {@link LockProvider} pour ShedLock utilisant une base de données relationnelle.
+     * Ce provider utilise {@link JdbcTemplate} pour interagir avec la table {@code shedlock} dans la base de données,
+     * qui stocke les informations de verrouillage. La configuration utilise l'heure de la base de données pour
+     * synchroniser les verrous.
+     *
+     * @param dataSource La source de données configurée pour l'application.
+     * @return Un {@link LockProvider} basé sur {@link JdbcTemplateLockProvider}.
+     */
+    @Bean
+    public LockProvider lockProvider(DataSource dataSource) {
+        return new JdbcTemplateLockProvider(JdbcTemplateLockProvider.
+                Configuration.builder()
+                .withJdbcTemplate(new JdbcTemplate(dataSource))
+                .usingDbTime() //Utilise l'heure de la base de données (compatible avec Postgres, MySQL, etc.)
+                .build());
+    }
+}
