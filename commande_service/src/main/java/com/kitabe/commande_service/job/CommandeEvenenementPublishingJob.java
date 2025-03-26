79e7e90a7b8d535a3e8fa05b4e33aac3f4750761
RayzoBirame <rayzobirame96@gmail.com>
2025-03-26 14:06:21 +0000

    nous avons ajouter SchedulingLock sur la methode publishCommandeEvenement pour s'assurer que la methode ne s'executera qu'une seule fois dans une instance sur un environnement de type distribuer (plusieurs microservices interconnectés)

--- commande_service/src/main/java/com/kitabe/commande_service/job/CommandeEvenenementPublishingJob.java
+++ commande_service/src/main/java/com/kitabe/commande_service/job/CommandeEvenenementPublishingJob.java
@@ -1,6 +1,8 @@
 package com.kitabe.commande_service.job;
 
 import com.kitabe.commande_service.domaine.CommandeEvenementService;
+import net.javacrumbs.shedlock.core.LockAssert;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -31,9 +33,13 @@ public class CommandeEvenenementPublishingJob {
      * Publie les événements de commande selon un calendrier défini par l'expression cron.
      * Cette méthode est exécutée périodiquement (selon la propriété {@code commande.publish-commande-evenement-Job-cron})
      * et appelle le service {@link CommandeEvenementService} pour publier les événements en attente.
+     * ShedLock garantit que cette méthode ne s'exécute que sur une seule instance à la fois dans un environnement distribué.
+     * La vérification {@link LockAssert#assertLocked()} assure que le verrou a été acquis avant l'exécution.
      */
     @Scheduled(cron = "${commande.publish-commande-evenement-Job-cron}")
+    @SchedulerLock(name="publishCommandeEvenement")
     void publishCommandeEvenement() {
+        LockAssert.assertLocked();
         log.info("Publication de commandeEvenement a {}", Instant.now());
         commandeEvenementService.publishCommandeOrder();
     }
