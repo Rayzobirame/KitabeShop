a9dc715e8afdfdf9cbff6b1fc6eecbf1fab8bed5
RayzoBirame <rayzobirame96@gmail.com>
2025-04-07 17:36:34 +0000

    Cette classe record est creer pour transferer les details d'une commande vers la couche presentation

---
+++ commande_service/src/main/java/com/kitabe/commande_service/domaine/model/CommandeDTO.java
@@ -0,0 +1,48 @@
+package com.kitabe.commande_service.domaine.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.kitabe.commande_service.domaine.CommandeStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+/**
+ * Représente un objet de transfert de données (DTO) pour une commande dans l'application KitabeShop.
+ * Ce record est utilisé pour transmettre les détails d'une commande à la couche de présentation,
+ * incluant des informations comme le numéro de commande, les articles, le client, l'adresse de livraison,
+ * le statut, et un montant total calculé.
+ *
+ * @param commandeNum   Le numéro unique de la commande.
+ * @param user          Le pseudo de l'utilisateur ayant passé la commande.
+ * @param items         L'ensemble des articles de la commande ({@link CommandeItems}).
+ * @param client        Les informations du client ({@link Client}).
+ * @param addresse      L'adresse de livraison ({@link Addresse}).
+ * @param status        Le statut de la commande ({@link CommandeStatus}).
+ * @param commentaire   Un commentaire optionnel associé à la commande.
+ * @param creationDate  La date et l'heure de création de la commande.
+ */
+public record CommandeDTO(
+        String commandeNum,
+        String user,
+        Set<CommandeItems> items,
+        Client client,
+        Addresse addresse,
+        CommandeStatus status,
+        String commentaire,
+        LocalDateTime creationDate
+
+) {
+    /**
+     * Calcule le montant total de la commande.
+     * Cette méthode additionne le coût de chaque article (prix unitaire multiplié par la quantité)
+     * pour obtenir le montant total de la commande. Le résultat est accessible uniquement en lecture
+     * via la sérialisation JSON.
+     *
+     * @return Le montant total de la commande sous forme de {@link BigDecimal}.
+     */
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    public BigDecimal getTotalMontant() {
+        return items.stream().map(item->item.prix().multiply(BigDecimal.valueOf(item.quantite()))).reduce(BigDecimal.ZERO,BigDecimal::add);
+    }
+}
