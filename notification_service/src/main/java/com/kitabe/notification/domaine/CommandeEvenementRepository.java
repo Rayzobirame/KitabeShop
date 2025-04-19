664b77e6d2ec56818234f69fe8566b7e4e4bf416
RayzoBirame <rayzobirame96@gmail.com>
2025-04-19 19:09:14 +0000

    Repository lier a l'entite CommandeEvenementEntite

---
+++ notification_service/src/main/java/com/kitabe/notification/domaine/CommandeEvenementRepository.java
@@ -0,0 +1,10 @@
+package com.kitabe.notification.domaine;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommandeEvenementRepository extends JpaRepository<CommandeEvenementEntite ,Long> {
+
+    boolean existsByEvenementId(String evenementId);
+}
