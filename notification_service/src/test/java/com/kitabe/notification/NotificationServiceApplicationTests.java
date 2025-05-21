9e6de9c87d73764b0e71e1053343241fc4ec70c5
RayzoBirame <rayzobirame96@gmail.com>
2025-05-21 19:39:48 +0000

    update

--- notification_service/src/test/java/com/kitabe/notification/NotificationServiceApplicationTests.java
+++ notification_service/src/test/java/com/kitabe/notification/NotificationServiceApplicationTests.java
@@ -9,7 +9,5 @@ import org.springframework.context.annotation.Import;
 class NotificationServiceApplicationTests extends AbstractIT {
 
     @Test
-    void contextLoads() {
-    }
-
+    void contextLoads() {}
 }
