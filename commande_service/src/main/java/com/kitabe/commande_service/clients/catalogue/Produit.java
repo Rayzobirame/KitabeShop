b68b02173ad34cd87fb573f3360e3fa769468f23
RayzoBirame <rayzobirame96@gmail.com>
2025-05-21 18:52:19 +0000

    update

--- commande_service/src/main/java/com/kitabe/commande_service/clients/catalogue/Produit.java
+++ commande_service/src/main/java/com/kitabe/commande_service/clients/catalogue/Produit.java
@@ -2,5 +2,4 @@ package com.kitabe.commande_service.clients.catalogue;
 
 import java.math.BigDecimal;
 
-public record Produit(String code , String nom, BigDecimal prix,String image_url, String description) {
-}
+public record Produit(String code, String nom, BigDecimal prix, String image_url, String description) {}
