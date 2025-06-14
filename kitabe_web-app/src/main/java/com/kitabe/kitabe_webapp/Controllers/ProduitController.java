577485838c97630270856f0a7f242f554ac88762
RayzoBirame <rayzobirame96@gmail.com>
2025-06-14 16:47:55 +0000

    update

--- kitabe_web-app/src/main/java/com/kitabe/kitabe_webapp/Controllers/ProduitController.java
+++ kitabe_web-app/src/main/java/com/kitabe/kitabe_webapp/Controllers/ProduitController.java
@@ -1,12 +1,22 @@
 package com.kitabe.kitabe_webapp.Controllers;
 
+import com.kitabe.kitabe_webapp.catalogue.CatalogueServiceClient;
+import com.kitabe.kitabe_webapp.catalogue.PagedResult;
+import com.kitabe.kitabe_webapp.catalogue.Produit;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ProduitController {
+
+    private CatalogueServiceClient catalogueServiceClient;
+
+    public ProduitController(CatalogueServiceClient catalogueServiceClient) {
+        this.catalogueServiceClient = catalogueServiceClient;
+    }
 
     @GetMapping
     String index(){
@@ -18,4 +28,13 @@ public class ProduitController {
         model.addAttribute("pageNum", pageNum);
         return "produits";
     }
+
+    @GetMapping("/api/produits")
+    @ResponseBody
+    PagedResult<Produit> produits(@RequestParam(name="page", defaultValue="1")int page, Model model){
+        model.addAttribute("pageNum", page);
+        return catalogueServiceClient.getProduits(page);
+    }
+
+
 }
