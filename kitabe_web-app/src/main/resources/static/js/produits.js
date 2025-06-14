3b08fe2821bf46cb4e3962b3482aa269c1bfa764
RayzoBirame <rayzobirame96@gmail.com>
2025-06-14 17:48:15 +0000

    Update

--- kitabe_web-app/src/main/resources/static/js/produits.js
+++ kitabe_web-app/src/main/resources/static/js/produits.js
@@ -1,21 +1,35 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('initData', (pageNum) => ({
         pageNum: pageNum,
-        produits:{
-            data:[]
+        produits: {
+            data: [],
+            pageNumero: 0,
+            totalPages: 0,
+            isPremier: false,
+            aPrecedent: false,
+            aSuivant: false,
+            aDernier: false
         },
+
         init() {
             console.log('Numero de page', this.pageNum);
-            this.loadProduit(this.pageNum)
+            this.loadProduit(this.pageNum);
         },
-        loadProduit(pageNum){
-            $.getJSON("http://localhost:8989/catalogue/api/produits?page=" + pageNum, (res) => {
-                console.log("Reponse produits:",res);
-                this.produits = res;
-            });
+
+        loadProduit(pageNum) {
+            if (pageNum < 1) pageNum = 1;
+            if (pageNum > this.produits.totalPages) pageNum = this.produits.totalPages;
+            fetch(`/api/produits?page=${pageNum}`)
+                .then(response => response.json())
+                .then(res => {
+                    console.log("Reponse produits:", res);
+                    this.produits = res; // res doit contenir pageNumero, totalPages, isPremier, etc.
+                })
+                .catch(error => console.error('Erreur lors du chargement des produits:', error));
         },
-        addAuPanier(produit){
+
+        addAuPanier(produit) {
             addProduitAuPanier(produit);
         }
-    }))
+    }));
 });
