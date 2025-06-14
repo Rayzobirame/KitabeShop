418ae1c1180e578bdffd26b65b1315a9af1033c1
RayzoBirame <rayzobirame96@gmail.com>
2025-06-14 16:46:48 +0000

    Update

--- kitabe_web-app/src/main/java/com/kitabe/kitabe_webapp/Controllers/CommandeController.java
+++ kitabe_web-app/src/main/java/com/kitabe/kitabe_webapp/Controllers/CommandeController.java
@@ -1,16 +1,39 @@
 package com.kitabe.kitabe_webapp.Controllers;
 
+import com.kitabe.kitabe_webapp.catalogue.Produit;
+import com.kitabe.kitabe_webapp.commande.*;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class CommandeController {
 
+    private final CommandeServiceClient commandeServiceClient;
+
+    public CommandeController(CommandeServiceClient commandeServiceClient) {
+        this.commandeServiceClient = commandeServiceClient;
+    }
+
     @GetMapping("/panier")
     public String panier(){
         return "panier";
+    }
+
+    @PostMapping("/api/commande")
+    @ResponseBody
+    CommandeConfirmationDTO creerCommande(@Valid @RequestBody CreerCommandeRequest request, @RequestHeader Map<String, ?> headers){
+        return commandeServiceClient.creerCommande(headers, request);
+    }
+
+    @GetMapping("/api/commande/{commandeNum}")
+    @ResponseBody
+    CommandeDTO getCommande(@RequestHeader(required = false) Map<String, ?> headers, @PathVariable String commandeNum) {
+        return commandeServiceClient.getCommande(headers, commandeNum);
     }
 
     @GetMapping("/commandes/{commandeNum}")
@@ -18,4 +41,16 @@ public class CommandeController {
         model.addAttribute("commandeNum", commandeNum);
         return "commande_details";
     }
+
+    @GetMapping("/api/commande")
+    @ResponseBody
+    List<CommandeSommaire> getCommandes(@RequestHeader(required = false) Map<String, ?> headers) {
+        return commandeServiceClient.getCommandes(headers);
+    }
+
+    @GetMapping("/commandes")
+    String showCommande(){
+        return "commandes";
+    }
+
 }
