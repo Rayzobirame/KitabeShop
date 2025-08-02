652f96bdeb22446921caaf3a2929e34095342221
RayzoBirame <rayzobirame96@gmail.com>
2025-08-02 13:08:14 +0000

    changement du prefix de kitabe a kitabeshop

--- kitabe_web-app/src/main/java/com/kitabe/kitabe_webapp/ApplicationProperties.java
+++ kitabe_web-app/src/main/java/com/kitabe/kitabe_webapp/ApplicationProperties.java
@@ -2,7 +2,7 @@ package com.kitabe.kitabe_webapp;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "kitabe")
-public record ApplicationProperties (
+@ConfigurationProperties(prefix = "kitabeshop")
+public record ApplicationProperties(
         String apiGatewayUrl
-){}
+) {}
