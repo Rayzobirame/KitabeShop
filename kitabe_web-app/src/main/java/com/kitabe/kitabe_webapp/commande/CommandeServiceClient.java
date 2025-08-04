88dfa63b9c3253525a91d78f8e2d0607606dee78
RayzoBirame <rayzobirame96@gmail.com>
2025-08-04 17:40:11 +0000

    Update

--- kitabe_web-app/src/main/java/com/kitabe/kitabe_webapp/commande/CommandeServiceClient.java
+++ kitabe_web-app/src/main/java/com/kitabe/kitabe_webapp/commande/CommandeServiceClient.java
@@ -1,22 +1,18 @@
 package com.kitabe.kitabe_webapp.commande;
 
-import com.kitabe.kitabe_webapp.catalogue.PagedResult;
-import com.kitabe.kitabe_webapp.catalogue.Produit;
-import org.springframework.http.ResponseEntity;
+import java.util.List;
+import java.util.Map;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.PostExchange;
-
-import java.util.List;
-import java.util.Map;
 
 public interface CommandeServiceClient {
 
     @PostExchange("/commande/api/commande")
-    CommandeConfirmationDTO creerCommande(@RequestHeader Map<String,?> headers, @RequestBody CreerCommandeRequest request);
+    CommandeConfirmationDTO creerCommande(
+            @RequestHeader Map<String, ?> headers, @RequestBody CreerCommandeRequest request);
 
     @GetExchange("/commande/api/commandes")
     List<CommandeSommaire> getCommandes(@RequestHeader Map<String, ?> headers);
