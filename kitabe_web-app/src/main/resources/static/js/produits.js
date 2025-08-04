8c3ea5fb986102b6ac93306f8c26874c9d4550c5
RayzoBirame <rayzobirame96@gmail.com>
2025-08-04 17:53:42 +0000

    La gestion de la pagination est gerer depuis le fichier pagination.html en fragment

--- kitabe_web-app/src/main/resources/static/js/produits.js
+++ kitabe_web-app/src/main/resources/static/js/produits.js
@@ -10,24 +10,17 @@ document.addEventListener('alpine:init', () => {
             aSuivant: false,
             aDernier: false
         },
-
         init() {
             console.log('Numero de page', this.pageNum);
             this.loadProduit(this.pageNum);
+            updatePanierItemCount();
         },
-
         loadProduit(pageNum) {
-            if (pageNum < 1) pageNum = 1;
-            if (pageNum > this.produits.totalPages) pageNum = this.produits.totalPages;
-            fetch(`/api/produits?page=${pageNum}`)
-                .then(response => response.json())
-                .then(res => {
-                    console.log("Reponse produits:", res);
-                    this.produits = res; // res doit contenir pageNumero, totalPages, isPremier, etc.
-                })
-                .catch(error => console.error('Erreur lors du chargement des produits:', error));
+            $.getJSON("/api/produits?page="+pageNum, (resp)=> {
+                console.log("Produits Resp:", resp)
+                this.produits= resp;
+            });
         },
-
         addAuPanier(produit) {
             addProduitAuPanier(produit);
         }
